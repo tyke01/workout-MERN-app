14776b9da7510649e012a6a0819519d33f01a81b
victor Macharia <Techvictor653@gmail.com>
2024-04-13 14:02:19 +0300

    tailwind initialized succesfullu, the backend is partially connected

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port : 5173,
+    proxy: {
+        '/api/workout': {
+            target: 'http://localhost:4000',
+            changeOrigin: true,
+            secure: false
+        }
+    }
+    
+},
 })
